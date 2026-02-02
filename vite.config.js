2fa1f520ce6530493a360a69d5fe76b55b137628
kk12837 <kk261188@gmail.com>
2026-02-02 19:09:40 +1030

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue-social-network/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
